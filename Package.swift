03bc92212e0ffd4a92fdb8a4ba4916173c7fac6c
Carlos O'Ryan <coryan@google.com>
2026-08-27 17:34:23 +0000

    cleanup: rename `packages/*` directories  (#548)
    Make the directories match their future split repositories.

--- Package.swift
+++ Package.swift
@@ -25,18 +25,18 @@ let package = Package(
       name: "GoogleCloudWorkflowsExecutionsV1", targets: ["GoogleCloudWorkflowsExecutionsV1"])
   ],
   dependencies: [
-    .package(path: "../../packages/auth"),
-    .package(path: "../../packages/gax"),
-    .package(path: "../../packages/wkt"),
+    .package(path: "../../packages/swift-google-auth"),
+    .package(path: "../../packages/swift-google-gax"),
+    .package(path: "../../packages/swift-google-wkt"),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
   ],
   targets: [
     .target(
       name: "GoogleCloudWorkflowsExecutionsV1",
       dependencies: [
-        .product(name: "GoogleCloudAuth", package: "auth"),
-        .product(name: "GoogleCloudGax", package: "gax"),
-        .product(name: "GoogleCloudWKT", package: "wkt"),
+        .product(name: "GoogleCloudAuth", package: "swift-google-auth"),
+        .product(name: "GoogleCloudGax", package: "swift-google-gax"),
+        .product(name: "GoogleCloudWKT", package: "swift-google-wkt"),
         .product(name: "Logging", package: "swift-log"),
       ],
     )
