16cefce672fba867bf92ed3524043620c1368abe
Carlos O'Ryan <coryan@google.com>
2026-09-16 13:55:59 -0400

    chore: update librarian circa 2026-09-16 (#913)
    Path parameter fields should be omitted in the body.

--- Sources/GoogleCloudWorkflowsExecutionsV1/Executions+Transport.swift
+++ Sources/GoogleCloudWorkflowsExecutionsV1/Executions+Transport.swift
@@ -203,9 +203,10 @@ extension Clients {
     public func cancelExecution(
       request: CancelExecutionRequest, options: GoogleCloudGax.RequestOptions
     ) async throws -> GoogleCloudWorkflowsExecutionsV1.Execution {
-      let (path, query, configure) = try {
+      let (path, query, configure, omitted) = try {
         () throws -> (
-          Swift.String, [URLQueryItem], (inout GoogleCloudGax._HTTPClientRequest) -> Void
+          Swift.String, [URLQueryItem], (inout GoogleCloudGax._HTTPClientRequest) -> Void,
+          [Swift.String]
         ) in
         if let candidate = try { () throws -> (Swift.String, [URLQueryItem])? in
           guard
@@ -225,7 +226,7 @@ extension Clients {
           ]
           return (path, query)
         }() {
-          return (candidate.0, candidate.1, { $0.setMethod(.POST) })
+          return (candidate.0, candidate.1, { $0.setMethod(.POST) }, ["name"])
         }
         var paths: [GoogleCloudGax.PathMismatch] = []
         do {
@@ -247,7 +248,7 @@ extension Clients {
         percentEncodedPath: path, query: query, options: options)
       configure(&req)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
-      try req.setBody(json: request)
+      try req.setBody(json: request, omitting: omitted)
       return try await req.rpc(
         GoogleCloudWorkflowsExecutionsV1.Execution.self, timeout: options.attemptTimeout
       ).get()
